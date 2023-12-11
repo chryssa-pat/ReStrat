7432eed7b86aa99ba3992c5b10594b5dfc0642dd
chryssa-pat <83291272+chryssa-pat@users.noreply.github.com>
2023-12-11 13:15:45 +0200

    Delete javascript directory

--- javascript/civilian.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById('logoutButton').addEventListener('click', function() {
-    var confirmLogout = confirm('Are you sure you want to logout?');
-    if (confirmLogout) {
-        // Redirect to another page
-        window.location.href = "main.html"; // Replace 'logout.php' with the actual URL you want to redirect to
-    }
-});
